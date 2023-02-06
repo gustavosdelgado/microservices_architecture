46616c72266c93a6c32c6df32eb1d31168f680b1
gustavosdelgado <gustavodelgado@protonmail.com>
2023-02-06 15:37:04 -0300

    adding authentication service

---
+++ micro-authentication/src/main/java/io/github/gustavosdelgado/microauthentication/MicroAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package io.github.gustavosdelgado.microauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAuthenticationApplication.class, args);
+	}
+
+}
