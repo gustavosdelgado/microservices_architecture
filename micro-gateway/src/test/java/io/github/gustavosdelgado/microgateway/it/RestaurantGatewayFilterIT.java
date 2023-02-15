3a1bf3e272090e8edba1afa316ee4af483a7b3ab
gustavosdelgado <gustavodelgado@protonmail.com>
2023-02-15 18:25:20 -0300

    added test scenarios on gateway integration test

--- micro-gateway/src/test/java/io/github/gustavosdelgado/microgateway/it/RestaurantGatewayFilterIT.java
+++ micro-gateway/src/test/java/io/github/gustavosdelgado/microgateway/it/RestaurantGatewayFilterIT.java
@@ -1,25 +1,17 @@
 package io.github.gustavosdelgado.microgateway.it;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import io.github.gustavosdelgado.microgateway.it.base.AuthTokenService;
 
-
-@Ignore
 @RunWith(SpringRunner.class)
-@SpringBootTest(properties = {
-    "spring.cloud.gateway.routes[0].id=test",
-    "spring.cloud.gateway.routes[0].uri=http://httpbin.org",
-    "spring.cloud.gateway.routes[0].predicates[0]=Path=/test/**",
-}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestaurantGatewayFilterIT {
 
     @Autowired
@@ -29,19 +21,19 @@ public class RestaurantGatewayFilterIT {
     private AuthTokenService tokenService;
 
     @Test
-    public void givenValidCredentialsWhenCallToRestaurantThenSucceeds() throws Exception {
+    public void givenValidCredentialsAndInvalidUriWhenCallToRestaurantThenFails() throws Exception {
         WebTestClient client = WebTestClient.bindToApplicationContext(this.context).build();
 
         var token = tokenService.generateToken();
 
-        client.get().uri("/test").header("Authorization", token).exchange().expectStatus().isUnauthorized();
+        client.get().uri("/notFound").header("Authorization", token).exchange().expectStatus().is4xxClientError();
     }
 
     @Test
     public void givenNoCredentialsWhenCallToRestaurantThenFails() throws Exception {
         WebTestClient client = WebTestClient.bindToApplicationContext(this.context)
-        .build();
-        client.get().uri("http://localhost:8081/restaurant").exchange().expectStatus().isUnauthorized();
+                .build();
+        client.get().uri("/restaurant").exchange().expectStatus().isUnauthorized();
     }
 
 }
