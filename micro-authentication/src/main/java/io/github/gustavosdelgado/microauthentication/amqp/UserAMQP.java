dc1ff7dfabbc50390206618f82582cdfa8654b87
gustavosdelgado <gustavodelgado@protonmail.com>
2023-02-17 17:20:47 -0300

    added amqp on auth service

---
+++ micro-authentication/src/main/java/io/github/gustavosdelgado/microauthentication/amqp/UserAMQP.java
@@ -0,0 +1,43 @@
+package io.github.gustavosdelgado.microauthentication.amqp;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserAMQP {
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory factory) {
+        return new RabbitAdmin(factory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initAdmin(RabbitAdmin admin) {
+        return event -> admin.initialize();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate createRabbitTemplate(ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter converter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange("user.exchange");
+    }
+}
