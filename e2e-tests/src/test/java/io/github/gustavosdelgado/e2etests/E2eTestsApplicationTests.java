f52b09c3d34126bcd59f3993167639f7be3e06d4
gustavosdelgado <gustavodelgado@protonmail.com>
2023-03-01 15:37:53 -0300

    added end to end service and container

---
+++ e2e-tests/src/test/java/io/github/gustavosdelgado/e2etests/E2eTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gustavosdelgado.e2etests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
