1e7f03451a49cda22cabe1d826a03711b0cb0409
gustavosdelgado <gustavodelgado@protonmail.com>
2023-03-03 14:12:35 -0300

    first end to end test scenario

--- e2e-tests/src/test/java/io/github/gustavosdelgado/e2etests/E2eTestsApplicationTests.java
+++ e2e-tests/src/test/java/io/github/gustavosdelgado/e2etests/E2eTestsApplicationTests.java
@@ -1,13 +1,33 @@
 package io.github.gustavosdelgado.e2etests;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+
 class E2eTestsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void createRestaurant() throws JSONException {
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json");
+
+		HttpEntity<String> request = new HttpEntity<>(buildRequestBody("restaurant4", "12456", "ROLE_RESTAURANT"),
+				headers);
+		ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8080/user", request,
+				String.class);
+		String json = response.getBody();
+		JSONObject jsonObject = new JSONObject(json);
+		String token = jsonObject.getString("token");
+	}
+
+	private String buildRequestBody(String login, String password, String role) {
+		return "{\"login\":\"" + login + "\",\"password\":\"" + password + "\",\"role\": \"" + role + "\"}";
 	}
 
 }
