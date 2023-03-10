5f4193d8643f0cbd8b182cf8494cf6d7f4cd6579
gustavosdelgado <gustavodelgado@protonmail.com>
2023-03-09 21:04:43 -0300

    dependencies, tests and github actions fixed

--- micro-authentication/src/test/java/io/github/gustavosdelgado/microauthentication/it/AuthenticationControllerIT.java
+++ micro-authentication/src/test/java/io/github/gustavosdelgado/microauthentication/it/AuthenticationControllerIT.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +30,12 @@ public class AuthenticationControllerIT {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @MockBean
+    private RabbitAdmin rabbitAdmin;
+
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
 
     @BeforeAll
     public void setup() {
