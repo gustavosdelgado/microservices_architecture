2a517ac045e6f97dba0e513d83445d08becb216c
gustavosdelgado <gustavodelgado@protonmail.com>
2023-03-20 10:27:55 -0300

    Revert "included todos"
    This reverts commit 1b40e5bbfed9423662fd0464e4bc3620eef78573.

--- commons-components/src/main/java/io/github/gustavosdelgado/library/domain/order/Order.java
+++ commons-components/src/main/java/io/github/gustavosdelgado/library/domain/order/Order.java
@@ -40,7 +40,5 @@ public class Order implements EntityDomain {
     public OrderWebResponse adaptToWebResponse() {
         return new OrderWebResponse(this.orderId, this.restaurantId);
     }
-    // TODO: implement all domain-related classes at common library
-    // TODO: implement all common services at common library
 
 }
