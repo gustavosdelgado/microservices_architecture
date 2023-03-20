3eacd6252af59e0465629e3d98f239b159682e30
gustavosdelgado <gustavodelgado@protonmail.com>
2023-03-20 14:03:40 -0300

    fixed user authorities implementation

--- commons-components/src/main/java/io/github/gustavosdelgado/library/domain/user/User.java
+++ commons-components/src/main/java/io/github/gustavosdelgado/library/domain/user/User.java
@@ -1,7 +1,7 @@
 package io.github.gustavosdelgado.library.domain.user;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,7 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
@@ -51,8 +50,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(Role.ROLE_CONSUMER.name()),
-                new SimpleGrantedAuthority(Role.ROLE_RESTAURANT.name()));
+        return Arrays.asList(role);
     }
 
     public Role getRole() {
