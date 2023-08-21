882bbc9d80a2e3e8c42f17ba20d8d1fb215ecdc3
Gustavo Delgado <gustavodelgado@protonmail.com>
2023-08-21 18:05:28 +1000

    removed circuit break configuration

--- micro-gateway/src/main/java/io/github/gustavosdelgado/microgateway/config/RoutesConfig.java
+++ micro-gateway/src/main/java/io/github/gustavosdelgado/microgateway/config/RoutesConfig.java
@@ -1,12 +1,19 @@
 package io.github.gustavosdelgado.microgateway.config;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.github.gustavosdelgado.microgateway.filter.RestaurantGatewayFilter;
+
+import java.time.Duration;
 
 @Configuration
 @EnableConfigurationProperties(UriConfiguration.class)
@@ -17,36 +24,24 @@ public class RoutesConfig {
         return builder.routes()
 				.route("restaurant_command", p -> p
 						.path("/restaurant/**")
-						.filters(f -> f.rewritePath("/restaurant/(?<segment>.*)", "/restaurant/${segment}")
-								.circuitBreaker(config -> config
-										.setName("restaurant_cb")))
+						.filters(f -> f.rewritePath("/restaurant/(?<segment>.*)", "/restaurant/${segment}"))
 						.uri(uriConfiguration.getRestaurantUri()))
 				.route("restaurant_query", p -> p
 						.path("/restaurant")
-						.filters(f -> f.filter(new RestaurantGatewayFilter())
-								.circuitBreaker(config -> config
-										.setName("restaurant_cb")))
+						.filters(f -> f.filter(new RestaurantGatewayFilter()))
 						.uri(uriConfiguration.getRestaurantUri()))
 				.route("order_command", p -> p
 						.path("/order/**")
-						.filters(f -> f.rewritePath("/order/(?<segment>.*)", "/order/${segment}")
-								.circuitBreaker(config -> config
-										.setName("order_cb")))
+						.filters(f -> f.rewritePath("/order/(?<segment>.*)", "/order/${segment}"))
 						.uri(uriConfiguration.getOrderUri()))
 				.route("order_query", p -> p
 						.path("/order")
-						.filters(f -> f.circuitBreaker(config -> config
-								.setName("order_cb")))
 						.uri(uriConfiguration.getOrderUri()))
 				.route("auth", p -> p
 						.path("/authenticate")
-						.filters(f -> f.circuitBreaker(config -> config
-								.setName("auth_cb")))
 						.uri(uriConfiguration.getAuthUri()))
 				.route("user", p -> p
 						.path("/user")
-						.filters(f -> f.circuitBreaker(config -> config
-								.setName("auth_cb")))
 						.uri(uriConfiguration.getAuthUri()))
                 .build();
     }
